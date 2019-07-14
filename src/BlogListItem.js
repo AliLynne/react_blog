42b9691a8e7929e38996827eaa0154844b19603d
AliLynne <alilynne.thompson@gmail.com>
2019-07-14 13:38:38 -0700

    more formatting

--- src/BlogListItem.js
+++ src/BlogListItem.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown';
+import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown/with-html';
+import htmlParser from 'react-markdown/plugins/html-parser';
 
-const BlogListItem = (props) => {
-  const post = props.entry.fields
-  console.log(post)
-  return (
-    <div className="container">
-      <li className="card">
-        <div className="card-content">
-          <h3 className="center">{post.title}</h3>
-          <blockquote>{post.description}</blockquote>
-          <ReactMarkdown source={post.body} />
-        </div>
+class BlogListItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      post: this.props.entry.fields,
+      htmlParser: null
+    }
+  }
+  
+  
+
+  
+  render() {
+    const post = this.state.post
+    console.log(post)
+    return (
+      <div className="container">
+        <li className="card">
+          <div className="card-content">
+            <img className="responsive-img" src={post.heroImage.fields.file.url} alt="hero"/>
+            <h3 className="center">{post.title}</h3>
+            <blockquote>{post.description}</blockquote>
+            <ReactMarkdown source={post.body} />
+          </div>
+          
+        </li>
         
-      </li>
-      
-    </div>
-  )
+      </div>
+    )
+  }
+  
 }
 
 export default BlogListItem
